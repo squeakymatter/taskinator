b1f739d6bc7257d7561f003da4b188448ef36fee
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2020-12-15 01:51:18 -0800

    add 4.1 snapshot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,11 @@
-//use the DOM to find the element object representation of <button> & assign it to variable buttonEl.
-
 var buttonEl = document.querySelector("#save-task");
-
-// use the DOM to find the element object representation of the tasks to do <ul>
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-//add event listener and change the text of the new task item dynamically using textContent property
-var createTaskHandler = function(){
-    //1. after button is clicked, create a new task item.
-    var listItemEl = document.createElement("li");
-    //2. style the new task item
-    listItemEl.className = "task-item";
-    //3. add the text dynamically using textContent property.
-    listItemEl.textContent = "This is a new task.";
-    //4. append the element to the task list.
-    tasksToDoEl.appendChild(listItemEl);
-    }
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
 
-    //use callTaskHandler as callback function
-    buttonEl.addEventListener("click", createTaskHandler);
-
+buttonEl.addEventListener("click", createTaskHandler);
